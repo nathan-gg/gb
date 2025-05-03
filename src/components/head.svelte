0c7f5ce2229902fd85e8eb121b7df082ea86669c
nathan-gg <nsgebreab@gmail.com>
2025-05-03 00:05:14 -0700

    made navbar close when outside clicked

--- src/components/head.svelte
+++ src/components/head.svelte
@@ -2,7 +2,8 @@
 	import { fly } from 'svelte/transition';
 	import { quintInOut } from 'svelte/easing';
 	import { sineInOut } from 'svelte/easing';
-	import { onMount } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
+	import { browser } from '$app/environment';
 
 	let showMenu = $state(false);
 	let menuElement = $state();
@@ -11,86 +12,98 @@
 		showMenu = !showMenu;
 	}
 
-	// function handleClickOutside(event) {
-	// 	// Only check if the menu is currently shown
-	// 	if (showMenu == true) {
-	// 		// Check if the click is outside the menu and not on the toggle button
-	// 		if (!menuElement.contains(event.target)) {
-	// 			showMenu = false;
-	// 		}
-	// 	}
-	// }
+	export function clickOutside(node) {
+		const handleClick = (event) => {
+			if (node && !node.contains(event.target) && !event.defaultPrevented) {
+				toggleMenu();
+			}
+		};
 
-	// onMount(() => {
-	// 	// Add click event listener to the document when component mounts
-	// 	document.addEventListener('click', handleClickOutside);
+		document.addEventListener('click', handleClick, true);
 
-	// 	// Clean up the event listener when component is destroyed
-	// 	return () => {
-	// 		document.removeEventListener('click', handleClickOutside);
-	// 	};
-	// });
+		return {
+			destroy() {
+				document.removeEventListener('click', handleClick, true);
+			}
+		};
+	}
+
+	let name = 'world';
+
+	function handleClickOutside(event) {
+		alert('Click outside!');
+	}
 </script>
 
-<section class="overflow-x-hidden">
-<div class="bg-primary p-4 px-8 h-16 sm:p-4 sm:px-16 w-full" >
-	
-	<div class="z-30 flex justify-between">
-		<a href="/home">
-			<img src="finalLogo.svg" class="h-8 justify-start " alt="Green Basil Logo" />
-		</a>
+<section class="overflow-x-hidden" use:clickOutside onclick_outside={handleClickOutside}>
+	<div class="bg-primary h-16 w-full p-4 px-8 sm:p-4 sm:px-16">
+		<div class="z-30 flex justify-between">
+			<a href="/home">
+				<img src="finalLogo.svg" class="h-8 justify-start" alt="Green Basil Logo" />
+			</a>
+			{#if showMenu}
+				<button onclick={toggleMenu} class:active={showMenu}>
+					<img src="closeMenu.svg" class="h-8" alt="Close Website Directory" />
+				</button>
+			{:else}
+				<button onclick={toggleMenu}>
+					<img src="menuBars.svg" class="h-8" alt="Open Website Directory" />
+				</button>
+			{/if}
+		</div>
+
 		{#if showMenu}
-			<button onclick={toggleMenu}  class:active={showMenu}>
-				<img src="closeMenu.svg" class="h-8 " alt="Close Website Directory" />
-			</button>
-		{:else}
-			<button onclick={toggleMenu}>
-				<img src="menuBars.svg" class="h-8 " alt="Open Website Directory" />
-			</button>
+			<div
+				id="MENU"
+				bind:this={menuElement}
+				transition:fly={{
+					delay: 0,
+					duration: 500,
+					easing: sineInOut,
+					x: 500,
+					y: 0
+				}}
+				class="bg-tertiary text-darkText fixed top-16 right-0 z-20 flex h-screen w-full flex-col p-12 sm:w-1/2 md:w-1/4"
+			>
+				<div
+					class=" font-instrument flex flex-col justify-between text-right text-4xl sm:text-2xl lg:text-4xl"
+				>
+					<a href="/home" onclick={toggleMenu}>Home</a>
+					<a href="/menus" onclick={toggleMenu}>Menus</a>
+					<a href="/order" onclick={toggleMenu}>Order Online</a>
+					<a href="/identity" onclick={toggleMenu}>The Basil Identity</a>
+					<a href="/careers" onclick={toggleMenu}>Careers</a>
+					<a href="/reservations" onclick={toggleMenu}>Reservations</a>
+					<a href="/contact" onclick={toggleMenu}>Contact Us</a>
+				</div>
+				<div
+					class="font-DMSans mt-6 grid grid-cols-2 text-xs sm:mt-12 md:grid-cols-1 lg:grid-cols-2"
+				>
+					<div class="col-span-1 flex flex-col md:mb-4">
+						<a
+							class="hover:underline"
+							href="https://www.instagram.com/greenbasilthai.yvr/"
+							target="_blank"
+							>Instagram
+						</a>
+
+						<a class="hover:underline" href="https://www.facebook.com/GreenBasil/" target="_blank"
+							>Facebook
+						</a>
+					</div>
+					<div class="text-darkText col-span-1 mb-8 flex flex-col">
+						<a
+							class="mb-2 underline"
+							href="https://www.google.com/maps/dir//4623+Kingsway,+Burnaby,+BC+V5H+2B3/@49.2291765,-123.0835327,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x54867658ed9537d5:0x9e538cb3bbfb2f7f!2m2!1d-123.0011323!2d49.2292058?entry=ttu&g_ep=EgoyMDI1MDIyNi4xIKXMDSoASAFQAw%3D%3D"
+							>4623 Kingsway, Burnaby, BC V5H 2B3</a
+						>
+
+						<p class="mb-2">5PM - 10PM WD/ 10:30PM WE</p>
+
+						<a class="mb-2 underline" href="tel:+16044391919">+1 (604) 439-1919</a>
+					</div>
+				</div>
+			</div>
 		{/if}
 	</div>
-
-	{#if showMenu}
-		<div
-			id="MENU"
-			
-			transition:fly={{
-				delay: 0,
-				duration: 500,
-				easing: sineInOut,
-				x: 500,
-				y: 0
-			}}
-			class="bg-tertiary text-darkText fixed top-16 right-0 flex h-screen w-full sm:w-1/2 md:w-1/4 flex-col p-12 z-20"
-		>
-			<div class=" font-instrument flex flex-col justify-between text-right text-4xl sm:text-2xl lg:text-4xl">
-				<a href="/home" onclick={toggleMenu}>Home</a>
-				<a href="/menus" onclick={toggleMenu}>Menus</a>
-				<a href="/order" onclick={toggleMenu}>Order Online</a>
-				<a href="/identity" onclick={toggleMenu}>The Basil Identity</a>
-				<a href="/careers" onclick={toggleMenu}>Careers</a>
-				<a href="/reservations" onclick={toggleMenu}>Reservations</a>
-				<a href="/contact" onclick={toggleMenu}>Contact Us</a>
-			</div>
-			<div class="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2 mt-6 sm:mt-12 font-DMSans text-xs ">
-				<div class="flex flex-col col-span-1 md:mb-4">
-					<a class="hover:underline" href="https://www.instagram.com/greenbasilthai.yvr/" target="_blank">Instagram </a>
-
-					<a class="hover:underline" href="https://www.facebook.com/GreenBasil/" target="_blank">Facebook </a>
-				</div>
-				<div class="col-span-1  text-darkText mb-8 flex flex-col ">
-					<a
-						class="underline mb-2"
-						href="https://www.google.com/maps/dir//4623+Kingsway,+Burnaby,+BC+V5H+2B3/@49.2291765,-123.0835327,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x54867658ed9537d5:0x9e538cb3bbfb2f7f!2m2!1d-123.0011323!2d49.2292058?entry=ttu&g_ep=EgoyMDI1MDIyNi4xIKXMDSoASAFQAw%3D%3D"
-						>4623 Kingsway, Burnaby, BC V5H 2B3</a
-					>
-
-					<p class="mb-2" >5PM - 10PM WD/ 10:30PM WE</p>
-
-					<a class="underline mb-2" href="tel:+16044391919">+1 (604) 439-1919</a>
-				</div>
-			</div>
-		</div>
-	{/if}
-</div>
 </section>
--- src/components/menus.svelte
+++ src/components/menus.svelte
@@ -40,92 +40,132 @@
 
 <section class="relative flex h-fit">
 	<div class="flex w-6/12 flex-auto flex-col bg-tertiary font-serif">
-		<div class="m-48 mt-12 flex flex-col">
+		<div class="m-48 mt-12 flex flex-col font-instrument">
 			<h1 class="text-darkText mt-24 text-6xl">{title}</h1>
 			<h1 class="text-lightText mb-12 text-4xl">{thaiTitle}</h1>
 
             <div class="font-DMSans text-3xl flex flex-col text-lightText">
-                <button class="hover:text-darkText  text-left" onclick={toggleDinner}>Dinner</button>
                 {#if showDinner}
-                <div 
-                transition:slide={{
-                    duration: 500,
-                    easing: quintInOut,
-                }}
-                class="text-xl flex flex-col">
-                    <a href="/full-menu#specialties" class="hover:text-darkText">Specialties</a>
-                    <a href="/full-menu#soups" class="hover:text-darkText">Soups</a>
-                    <a href="/full-menu#appetizers" class="hover:text-darkText">Appetizers</a>
-                    <a href="/full-menu#salad" class="hover:text-darkText">Salad</a>
-                    <a href="/full-menu#curry" class="hover:text-darkText">Curry</a>
-                    <a href="/full-menu#noodles" class="hover:text-darkText">Noodles</a>
-                    <a href="/full-menu#vegetables" class="hover:text-darkText">Vegetables</a>
-                    <a href="/full-menu#stir-fried" class="hover:text-darkText">Stir Fried</a>
-                    <a href="/full-menu#fried-rice" class="hover:text-darkText">Fried Rice</a>
-                    <a href="/full-menu#desserts" class="hover:text-darkText">Desserts</a>
+					<button class="text-darkText text-left" onclick={toggleDinner}>Dinner</button>
+					<div
+						transition:slide={{
+							duration: 250,
+							easing: quintInOut
+						}}
+						class="flex flex-col text-lg"
+					>
+						
+						<a href="/full-menu#soups" class="hover:text-darkText mb-2 inline-block">Soups</a>
+						<a href="/full-menu#appetizers" class="hover:text-darkText mb-2 inline-block"
+							>Appetizers</a
+						>
+						<a href="/full-menu#salad" class="hover:text-darkText mb-2 inline-block">Salad</a>
+						<a href="/full-menu#curry" class="hover:text-darkText mb-2 inline-block">Curry</a>
+						<a href="/full-menu#stir-fried" class="hover:text-darkText mb-2 inline-block"
+							>Stir Fried</a
+						>
+						<a href="/full-menu#noodles" class="hover:text-darkText mb-2 inline-block">Noodles</a>
+						<a href="/full-menu#vegetables" class="hover:text-darkText mb-2 inline-block"
+							>Vegetables</a
+						>
+						
+						<a href="/full-menu#rice" class="hover:text-darkText mb-2 inline-block"
+							>Rice</a
+						>
+						<a href="/full-menu#desserts" class="hover:text-darkText mb-2 inline-block">Desserts</a>
+						<a href="/full-menu#specialties" class="hover:text-darkText mb-2 inline-block">Specialties</a>
+					</div>
+				{:else}
+					<button class="hover:text-darkText text-left" onclick={toggleDinner}>Dinner</button>
+				{/if}
 
-                </div>
-                {/if}
-                <button class="hover:text-darkText text-left" onclick={toggleHH}>Happy Hour</button>
-                {#if showHH}
-                <div 
-                transition:slide={{
-                    duration: 500,
-                    easing: quintInOut,
-                }}
-                class="text-xl flex flex-col">
-                    <a href="/full-menu#food-hh" class="hover:text-darkText">Food</a>
-                    <a href="/full-menu#draught-beer-hh" class="hover:text-darkText">Draught Beer</a>
-                    <a href="/full-menu#highballs-hh" class="hover:text-darkText">High Balls</a>
-                    <a href="/full-menu#specialties-hh" class="hover:text-darkText">Specialties</a>
-                    <a href="/full-menu#featured-cocktails-hh" class="hover:text-darkText">Featured Cocktails</a>
-                    <a href="/full-menu#wine-hh" class="hover:text-darkText">Wine</a>
-                </div>
-                {/if}
-                <button class="hover:text-darkText text-left" onclick={toggleBar}>Bar</button>
-                {#if showBar}
-                <div 
-                transition:slide={{
-                    duration: 500,
-                    easing: quintInOut,
-                }}
-                class="text-xl flex flex-col">
-                    <a href="/full-menu#ctsd" class="hover:text-darkText">Coffee, Tea & Soft Drinks</a>
-                    <a href="/full-menu#drinks" class="hover:text-darkText">Drinks</a>
-                    <a href="/full-menu#non-spirited" class="hover:text-darkText">Non-Spirited</a>
-                    <a href="/full-menu#cocktails" class="hover:text-darkText">Cocktails</a>
-                    <a href="/full-menu#signature-cocktails" class="hover:text-darkText">Signature Cocktails</a>
-                    <a href="/full-menu#classic-cocktails" class="hover:text-darkText">Classic Cocktails</a>
-                    <a href="/full-menu#draught-beer" class="hover:text-darkText">Draught Beer</a>
-                    <a href="/full-menu#domestic-beer" class="hover:text-darkText">Domestic Beer</a>
-                    <a href="/full-menu#imported-beer" class="hover:text-darkText">Imported Beer</a>
-                    <a href="/full-menu#cider" class="hover:text-darkText">Cider</a>
-                    <a href="/full-menu#vodka" class="hover:text-darkText">Vodka</a>
-                    <a href="/full-menu#gin" class="hover:text-darkText">Gin</a>
-                    <a href="/full-menu#rum" class="hover:text-darkText">Rum</a>
-                    <a href="/full-menu#tequila" class="hover:text-darkText">Tequila</a>
-                    <a href="/full-menu#whiskey" class="hover:text-darkText">Whisk(e)y</a>
-                    <a href="/full-menu#scotch" class="hover:text-darkText">Scotch</a>
-                    <a href="/full-menu#brandy" class="hover:text-darkText">Brandy</a>
-                    <a href="/full-menu#aperitif-digestif" class="hover:text-darkText">Aperitif & Digestif</a>
-                    <a href="/full-menu#vermouth-liquers" class="hover:text-darkText">Vermouth & Liquers</a>
-                    <a href="/full-menu#sct" class="hover:text-darkText">Specialty Coffee & Tea</a>
-                </div>
-                {/if}
-                <button class="hover:text-darkText text-left" onclick={toggleWine}>Wine</button>
-                {#if showWine}
-                <div 
-                transition:slide={{
-                    duration: 500,
-                    easing: quintInOut,
-                }}
-                class="text-xl flex flex-col">
-                    <a href="/full-menu#featured-wine" class="hover:text-darkText">Featured Wine</a>
-                    <a href="/full-menu#white-wines" class="hover:text-darkText">White Wines</a>
-                    <a href="/full-menu#rose" class="hover:text-darkText">Rosé</a>
-                    <a href="/full-menu#red-wines" class="hover:text-darkText">Red Wines</a>
-                </div>
-                {/if}
+				{#if showHH}
+					<button class="text-darkText mt-4 text-left" onclick={toggleHH}>Happy Hour</button>
+					<div
+						transition:slide={{
+							duration: 250,
+							easing: quintInOut
+						}}
+						class="flex flex-col text-xl"
+					>
+						<a href="/full-menu#happy-hour" class="hover:text-darkText mb-2 inline-block">Happy Hour Menu</a>
+					</div>
+				{:else}
+					<button class="hover:text-darkText mt-4 text-left" onclick={toggleHH}>Happy Hour</button>
+				{/if}
+
+				{#if showBar}
+					<button class="text-darkText mt-4 text-left" onclick={toggleBar}>Bar</button>
+					<div
+						transition:slide={{
+							duration: 250,
+							easing: quintInOut
+						}}
+						class="flex flex-col text-xl"
+					>
+						<a href="/full-menu#ctsd" class="hover:text-darkText mb-2 inline-block"
+							>Coffee, Tea & Soft Drinks</a
+						>
+						
+						<a href="/full-menu#non-spirited" class="hover:text-darkText mb-2 inline-block"
+							>Non-Spirited</a
+						>
+						<a href="/full-menu#signature-cocktails" class="hover:text-darkText mb-2 inline-block"
+							>Signature Cocktails</a
+						>
+						<a href="/full-menu#classic-cocktails" class="hover:text-darkText mb-2 inline-block"
+							>Classic Cocktails</a
+						>
+						
+						<a href="/full-menu#beer" class="hover:text-darkText mb-2 inline-block"
+							>Beer</a
+						>
+						
+						<a href="/full-menu#cider" class="hover:text-darkText mb-2 inline-block">Cider</a>
+						<a href="/full-menu#vodka" class="hover:text-darkText mb-2 inline-block">Vodka</a>
+						<a href="/full-menu#gin" class="hover:text-darkText mb-2 inline-block">Gin</a>
+						<a href="/full-menu#rum" class="hover:text-darkText mb-2 inline-block">Rum</a>
+						<a href="/full-menu#tequila" class="hover:text-darkText mb-2 inline-block">Tequila</a>
+						<a href="/full-menu#whisky" class="hover:text-darkText mb-2 inline-block">Whisky</a>
+						<a href="/full-menu#scotch" class="hover:text-darkText mb-2 inline-block">Scotch</a>
+						<a href="/full-menu#brandy" class="hover:text-darkText mb-2 inline-block">Brandy</a>
+						<a href="/full-menu#aperitif-digestif" class="hover:text-darkText mb-2 inline-block"
+							>Aperitif & Digestif</a
+						>
+						<a href="/full-menu#vermouth-liqueurs" class="hover:text-darkText mb-2 inline-block"
+							>Vermouth & Liquers</a
+						>
+						<a href="/full-menu#sct" class="hover:text-darkText mb-2 inline-block"
+							>Specialty Coffee & Tea</a
+						>
+					</div>
+				{:else}
+					<button class="hover:text-darkText mt-4 text-left" onclick={toggleBar}>Bar</button>
+				{/if}
+
+				{#if showWine}
+					<button class="text-darkText mt-4 text-left" onclick={toggleWine}>Wine</button>
+					<div
+						transition:slide={{
+							duration: 250,
+							easing: quintInOut
+						}}
+						class="flex flex-col text-xl"
+					>
+						<a href="/full-menu#featured-wine" class="hover:text-darkText mb-2 inline-block"
+							>Featured Wine</a
+						>
+						<a href="/full-menu#white-wines" class="hover:text-darkText mb-2 inline-block"
+							>White Wines</a
+						>
+						<a href="/full-menu#rose" class="hover:text-darkText mb-2 inline-block">Rosé</a>
+						<a href="/full-menu#red-wines" class="hover:text-darkText mb-2 inline-block"
+							>Red Wines</a
+						>
+					</div>
+				{:else}
+					<button class="hover:text-darkText mt-4 text-left" onclick={toggleWine}>Wine</button>
+				{/if}
                 
 			</div>
 
