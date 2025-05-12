5d922624f1e8b3f5efaad402a8f6f904d0385a68
nathan-gg <nsgebreab@gmail.com>
2025-05-12 15:59:54 -0700

    changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,33 @@
 
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// const dev = process.argv.includes('dev')
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://svelte.dev/docs/kit/integrations
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		adapter: adapter({
+// 			fallback: '404.html'
+// 		}),
+// 		paths: {
+// 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+// 		}
+// 	}
+// };
+
+// export default config;
+
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev')
+const dev = process.argv.includes('dev');
+// Get the repository name for GitHub Pages
+const repo = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +40,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: dev ? '' : `/${repo}`
 		}
 	}
 };
