e9a81bf84257cd6f8aa150b3e985a1565eedb2d5
nathan-gg <nsgebreab@gmail.com>
2025-05-12 21:16:06 -0700

    changed adapter to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -45,7 +45,7 @@
 // 	export default config;
 	
 
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
