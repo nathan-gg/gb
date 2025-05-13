a8fa4ae8e3f7f30680ffd9ee6bddfb40180987a2
nathan-gg <nsgebreab@gmail.com>
2025-05-12 20:05:28 -0700

    static site adapter

--- svelte.config.js
+++ svelte.config.js
@@ -45,7 +45,7 @@
 // 	export default config;
 	
 
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
