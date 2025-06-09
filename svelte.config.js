296e2dd614c5dd457790f6202b41b15e1b56db44
Nathan Gebreab <nathan_gebreab@sfu.ca>
2025-06-09 14:42:26 -0700

    changed adapter to cloudflare from vercel

--- svelte.config.js
+++ svelte.config.js
@@ -45,7 +45,7 @@
 // 	export default config;
 	
 
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
